5b5f3c4ca4ed4139c66ffefca6cd147c2cd03454
cecgil <dudigil6@gmail.com>
2025-08-21 23:49:53 -0300

    Initial commit - Entendendo componentes e styles inicialmente com App + styles

--- App.tsx
+++ App.tsx
@@ -1,20 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from "react-native"
+
+import { styles } from "./styles"
 
 export default function App() {
-  return (
+  return(
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Hello, World!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
